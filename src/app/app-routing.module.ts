7a71d6f2b280be59b7af18d445407eddb49f67cd
jo.cedeno <jo.cedeno@duocuc.cl>
2023-10-08 16:03:48 -0300

    actividad 1.2.4 lista

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'fecha',
+    loadChildren: () => import('./fecha/fecha.module').then( m => m.FechaPageModule)
   },
+
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,22 +1,48 @@
-import { Component, ViewChild} from '@angular/core';
-import { IonDatetime } from '@ionic/angular';
+import { Component} from '@angular/core';
+import { format, parseISO } from 'date-fns';
+import { AlertController } from '@ionic/angular';
 
 @Component({
+  
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  fechaNacimiento: string = ''; // Propiedad para almacenar la fecha seleccionada
+  showPicker = false;
+  dateValue = format(new Date(), 'yyyy-MM-dd') + 'T09:00:00.000Z';
+  formattedString = '';
 
-  @ViewChild('myDatetime') myDatetime: IonDatetime | undefined;
-
-  constructor() {}
-
-  confirmSelection() {
-    if (this.myDatetime) {
-      this.myDatetime.confirm(true); // Cierra el calendario y confirma la selección
-    }
+  constructor(private alertController: AlertController){
+    this.setToday();
   }
 
+  setToday(){
+    this.formattedString = format(parseISO(format(new Date(), 'yyyy-MM-dd') + 'T09:00:00.000Z'), 'dd MMM, yyyy');
+  }
+
+  dateChanged(value: any){
+    console.log(value);
+    this.dateValue = value;
+    this.formattedString = format(parseISO(value), 'dd MMM, yyyy');
+    this.showPicker = false;
+  }
+
+  nombre: string = '';
+  apellido: string = '';
+
+  async limpiarValores() {
+    this.nombre = '';
+    this.apellido = '';
+  }
+
+  async mostrarValores() {
+    const alert = await this.alertController.create({
+      header: 'Valores',
+      message: `Nombre: ${this.nombre}\n Apellido: ${this.apellido}`,
+      buttons: ['OK']
+    });
+  
+    await alert.present();
+  }
 }
